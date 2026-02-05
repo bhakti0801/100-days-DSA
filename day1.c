55c134ab09c0540745842f4e08a0c7c85d05ae7b
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2026-02-05 23:23:45 +0530

    Create day1.c

---
+++ day1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos, x;
+    int a[100];   // assuming max size is enough
+
+    // Read number of elements
+    scanf("%d", &n);
+
+    // Read array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Read position (1-based)
+    scanf("%d", &pos);
+
+    // Read element to insert
+    scanf("%d", &x);
+
+    // Shift elements to the right
+    for (int i = n; i >= pos; i--) {
+        a[i] = a[i - 1];
+    }
+
+    // Insert element
+    a[pos - 1] = x;
+    n++;
+
+    // Print updated array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
